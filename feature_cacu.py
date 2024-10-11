7655225331dcbda19426ddc3a2d624424def02e0
xinz <c110118152@nkust.edu.tw>
2024-10-11 17:12:32 +0800

    add_blinking

--- feature_cacu.py
+++ feature_cacu.py
@@ -38,6 +38,30 @@ class feature_calculate:
         normalize = normal / 2
         distance = (np.sqrt((x1 - x2)**2 + (y1 - y2)**2)) / normalize
         return distance
+    
+    def euclidean_distance(point1, point2):
+        return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+    # 根據兩眼垂直與水平距離比例檢測是否眨眼
+    
+    def detect_blink(landmarks, eye_indices, imgWidth, imgHeight):
+        # 提取眼睛的6個關鍵點座標
+        eye = [(int(landmarks[i].x * imgWidth), int(landmarks[i].y * imgHeight)) for i in eye_indices]
+
+        # 計算眼睛垂直和水平距離
+        horizontal_distance = feature_calculate.euclidean_distance(eye[0], eye[3])
+        vertical_distance_1 = feature_calculate.euclidean_distance(eye[1], eye[5])
+        vertical_distance_2 = feature_calculate.euclidean_distance(eye[2], eye[4])
+        
+        # 平均垂直距離
+        vertical_distance = (vertical_distance_1 + vertical_distance_2) / 2.0
+
+        # 計算眼睛的閉合比例
+        blink_ratio = vertical_distance / horizontal_distance
+
+        # 若閉合比例小於一定值，視為眨眼
+        return blink_ratio < 0.2
+    
+
 
     def draw_results_on_image(image, rg_distance, lf_distance, rg_angle, lf_angle):
         # 顯示角度
@@ -47,3 +71,5 @@ class feature_calculate:
 
         cv2.putText(image, f"Right Eye Angle: {rg_angle:.2f}", (50, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
         cv2.putText(image, f"Left Eye Angle: {lf_angle:.2f}", (50, 220), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+        
+    
